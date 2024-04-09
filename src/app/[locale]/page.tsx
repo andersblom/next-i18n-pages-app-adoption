facdf1705c7b7f0c61d40dfbdab75db2c8a46e67
Anders Blom <anders@andersblom.dk>
2024-04-09 14:56:23 -0500

    allow implicit any

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,7 +4,7 @@ import { useParams } from "next/navigation";
 
 export default function Home() {
   const params = useParams();
-  const locale = params.locale;
+  const locale = params?.locale;
   return (
     <main>
       <div>Home (app) - Client side rendered</div>
